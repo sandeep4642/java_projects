1520d309aec038a0046765e047ed60322d103340
Sandeep <sandeep@fifthgentech.local>
2018-04-16 17:19:32 +0530

    class name is changed

--- sample.java
+++ sample.java
@@ -1,12 +1,5 @@
-public class Hello{
-	
-	public static void main(string[] args){
-		
-		for(int i=1;i<=10;i++){
-			
-			System.out.println("Hello world.."+i);
-			
-		}
-	}
-	
+public class sample {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
 }
